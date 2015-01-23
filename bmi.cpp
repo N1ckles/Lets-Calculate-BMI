c31fc1e5eb9d3f4108547b09452e0c06f3074773
Nicklas Vedsted <nicklasvedsted@hotmail.com>
2015-01-24 00:06:35 +0100

    Add BMI calculator for C++
     - Add bmi.cpp

---
+++ bmi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	if(argc!=3){
+		cout<<"Usage: "<<argv[0]<<" <weight> <height>"<<endl;
+	}else{
+		double height=atof(argv[2]);
+		double weight=atof(argv[1]);
+		double bmi=weight * (height * height);
+
+		cout<<"Your BMI is: "<<bmi<<endl;
+		if(bmi <= 18.5){
+			cout<<"You're underweight."<<endl;
+		}else if(bmi <= 25){
+			cout<<"You're healthy."<<endl;
+		}else if(bmi <= 30){
+			cout<<"You're overweight."<<endl;
+		}else{
+			cout<<"Holy McShizzle, you're freaking whale!"<<endl;
+		}
+	}
+	return 0;
+}
